5c9b5047bba2d546b397ed512473202ae7d03957
stefano-malacrino <38613112+stefano-malacrino@users.noreply.github.com>
2025-01-07 09:28:51 +0000

    Add methods to SoftwareRenderContext and SoftwareRenderBackend

--- src/softwarerenderbackend.cpp
+++ src/softwarerenderbackend.cpp
@@ -9,5 +9,7 @@ PYBIND11_MODULE(softwarerenderbackend, m)
     py::module_::import("pixelengine");
     
     py::class_<SoftwareRenderBackend, RenderBackend>(m, "SoftwareRenderBackend")
-        .def(py::init<>());
+        .def(py::init<RenderBackend::ImageFormatType, size_t>(),
+            py::arg("image_format_type") = RenderBackend::ImageFormatType::RGBA,
+            py::arg("tile_cache_size_bytes") = 256000000);
 }
--- src/softwarerendercontext.cpp
+++ src/softwarerendercontext.cpp
@@ -10,5 +10,8 @@ PYBIND11_MODULE(softwarerendercontext, m)
 
     py::class_<SoftwareRenderContext, RenderContext>(m, "SoftwareRenderContext")
         .def(py::init<>())
-        .def(py::init<size_t, size_t>(), py::arg("width"), py::arg("height"));
+        .def(py::init<size_t, size_t>(), py::arg("width"), py::arg("height"))
+        .def_property("number_of_worker_threads",
+            py::overload_cast<>(&SoftwareRenderContext::numberOfWorkerThreads, py::const_),
+            py::overload_cast<size_t>(&SoftwareRenderContext::numberOfWorkerThreads));
 }
